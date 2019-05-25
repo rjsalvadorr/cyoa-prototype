f14078d335978e36bfe9349e9ed33d401fcc50bb
RJ Salvador <rjsalvadorr@gmail.com>
2019-05-25 13:41:29 -0400

    Basic CYOA functionality is complete

--- .vuepress/components/PageDisplay.vue
+++ .vuepress/components/PageDisplay.vue
@@ -1,23 +1,28 @@
 <template>
-  <div class="page-display">
-    <div class="snippets">
-      <div v-for="snippet in cyoaData.snippets" :class="`snippet snippet--${snippet.id}`">
-        <h2 v-if="snippet.title">{{snippet.title}}</h2>
-        <p>{{snippet.text}}</p>
-      </div>
+  <div class="cyoa">
+    <div v-if="getCurrentData.snippet" :class="`snippet snippet--${getCurrentData.snippet.id}`">
+      <h2 v-if="getCurrentData.snippet.title">{{getCurrentData.snippet.title}}</h2>
+      <p>{{getCurrentData.snippet.text}}</p>
     </div>
-    <div class="choices">
-      <div v-for="choice in cyoaData.choices" :class="`choice choice--${choice.id}`">
-        <p>{{choice.text}}</p>
-      </div>
+    <div v-if="getCurrentData.choices" class="choices">
+        <button v-for="link in getCurrentData.choices.links"
+          type="button"
+          :class="`btn btn--link btn--${link.dest}`"
+          v-bind:key="`${getCurrentData.choices.id}-${link.dest}`"
+          :data-dest="link.dest"
+          v-on:click="changeSnippet"
+        >{{link.label}}</button>
     </div>
     <hr>
-    <code>{{JSON.stringify(cyoaData, null, 2)}}</code>
+    <code>{{JSON.stringify(getCurrentData, null, 2)}}</code>
   </div>
 </template>
 
 <script>
+import filter from 'lodash/filter';
+import isEmpty from 'lodash/isEmpty';
 import * as cyoaData from './cyoa-data.json';
+
 export default {
   name: 'PageDisplay',
   props: {
@@ -31,12 +36,47 @@ export default {
     }
   },
   computed: {
-    getCurrentYear: () => {
-      return new Date().getFullYear();
-    },
+    getCurrentData: function() {
+      const currentId = this.currentSnippet;
+
+      const cSnippet = filter(this.cyoaData.snippets, (o) => { return o.id === currentId; });
+      const cChoices = filter(this.cyoaData.choices, (o) => { return o.id === currentId });
+      
+      console.log(
+        currentId,
+        this.cyoaData.snippets,
+        this.cyoaData.choices,
+        cSnippet,
+        cChoices,
+      );
+      return {
+        snippet: isEmpty(cSnippet) ? null : cSnippet[0],
+        choices: isEmpty(cChoices) ? null : cChoices[0],
+      };
+    }
+  },
+  methods: {
+    changeSnippet: function(evt) {
+      const destFromButton = parseInt(evt.target.dataset.dest);
+      if(destFromButton) {
+        this.currentSnippet = destFromButton;
+      }
+    }
   },
 }
 </script>
 
 <style scoped lang="scss">
+  @import "../styles/vars.scss";
+
+  .cyoa {
+    .btn {
+      margin-right: 10px;
+      padding: 10px;
+      height: 50px;
+      border-radius: 4px;
+      background-color: $palette-secondary;
+      color: #eee;
+    }
+  }
 </style>
